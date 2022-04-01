12762d668d5fe36537e903eff3e251a4ec2d9635
fatih elçi <fatih.elci2410@gmail.com>
2022-04-01 12:24:49 +0300

    NoteAppFireBase

---
+++ NoteAppFireBase/NoteApp/NotDetayViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NotDetayViewController.swift
+//  NoteApp
+//
+//  Created by fatih on 24.02.2022.
+//
+
+import UIKit
+import Firebase
+
+class NotDetayViewController: UIViewController {
+
+    @IBOutlet weak var textfieldDersAdi: UITextField!
+    @IBOutlet weak var textfieldNot1: UITextField!
+    @IBOutlet weak var textfieldNot2: UITextField!
+    
+    var not:Notlar?
+    var ref:DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let n = not {
+            textfieldDersAdi.text = n.ders_adi
+            textfieldNot1.text = String(n.not1!)
+            textfieldNot2.text = String(n.not2!)
+        }
+        
+        ref = Database.database().reference()
+    }
+    
+    @IBAction func notSil(_ sender: Any) {
+        if let n = not {
+            notSil(not_id: n.not_id!)
+        }
+    }
+    
+    @IBAction func notGuncelle(_ sender: Any) {
+        if let n = not,let ad = textfieldDersAdi.text,let not1 = textfieldNot1.text,let not2 = textfieldNot2.text{
+            if let n1 = Int(not1),let n2 = Int(not2){
+                notGuncelle(not_id: n.not_id!, ders_adi: ad, not1: n1, not2: n2)
+            }
+        }
+
+    }
+    func notSil(not_id:String){
+        
+        ref.child("notlar").child(not_id).removeValue()
+        
+    }
+    func notGuncelle(not_id:String,ders_adi:String,not1:Int,not2:Int){
+        
+        ref.child("notlar").child(not_id).updateChildValues(["ders_adi":ders_adi,"not1":not1,"not2":not2])
+        
+        
+    }
+}
---
+++ NoteAppFireBase/NoteApp/NotEkleViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NotEkleViewController.swift
+//  NoteApp
+//
+//  Created by fatih on 24.02.2022.
+//
+
+import UIKit
+import Firebase
+
+class NotEkleViewController: UIViewController {
+
+    @IBOutlet weak var textfieldDersAdi: UITextField!
+    @IBOutlet weak var textfieldNot1: UITextField!
+    @IBOutlet weak var textfieldNot2: UITextField!
+    
+    var ref:DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ref = Database.database().reference()
+    }
+    
+
+    @IBAction func notEkle(_ sender: Any) {
+        if let ad = textfieldDersAdi.text,let not1 = textfieldNot1.text,let not2 = textfieldNot2.text{
+            if let n1 = Int(not1),let n2 = Int(not2){
+                notEkle(ders_adi: ad, not1: n1, not2: n2)
+            }
+        }
+    }
+    func notEkle(ders_adi:String,not1:Int,not2:Int){
+        let dict:[String:Any] = ["ders_adi":ders_adi,"not1":not1,"not2":not2]
+        
+        let newRef = ref.child("notlar").childByAutoId()
+        
+        newRef.setValue(dict)
+        
+    }
+
+}
