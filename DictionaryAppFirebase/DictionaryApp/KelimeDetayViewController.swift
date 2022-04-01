fb80e0696dadf911de105176b3855865274d1861
fatih elçi <fatih.elci2410@gmail.com>
2022-04-01 12:31:05 +0300

    DictionaryAppFirebase

---
+++ DictionaryAppFirebase/DictionaryApp/KelimeDetayViewController.swift
@@ -0,0 +1,26 @@
+//
+//  KelimeDetayViewController.swift
+//  DictionaryApp
+//
+//  Created by fatih on 24.02.2022.
+//
+
+import UIKit
+
+class KelimeDetayViewController: UIViewController {
+    @IBOutlet weak var ingilizceLabel: UILabel!
+    
+    @IBOutlet weak var turkceLabel: UILabel!
+    
+    var kelime:Kelimeler?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let k = kelime {
+            ingilizceLabel.text = k.ingilizce
+            turkceLabel.text = k.turkce
+        }
+    }
+
+}
---
+++ DictionaryAppFirebase/DictionaryApp/Kelimeler.swift
@@ -0,0 +1,26 @@
+//
+//  Kelimeler.swift
+//  DictionaryApp
+//
+//  Created by fatih on 24.02.2022.
+//
+
+import Foundation
+
+
+class Kelimeler {
+    var kelime_id:String?
+    var ingilizce:String?
+    var turkce:String?
+    
+    init(){
+        
+    }
+    
+    init(kelime_id:String,ingilizce:String,turkce:String){
+        self.kelime_id = kelime_id
+        self.ingilizce = ingilizce
+        self.turkce = turkce
+        
+    }
+}
