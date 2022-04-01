e253a62ca7196d9cc33c3071e3705fe7636d1d16
fatih elçi <fatih.elci2410@gmail.com>
2022-04-01 12:17:27 +0300

    MovieApplicationFirebasex

---
+++ MovieApplicationFirebase/MovieApp/FilmDetayViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FilmDetayViewController.swift
+//  MovieApp
+//
+//  Created by fatih on 26.02.2022.
+//
+
+import UIKit
+
+class FilmDetayViewController: UIViewController {
+
+    @IBOutlet weak var imageViewFilmResim: UIImageView!
+    @IBOutlet weak var labelFilmAd: UILabel!
+    @IBOutlet weak var labelFimYil: UILabel!
+    @IBOutlet weak var labelFilmKategori: UILabel!
+    @IBOutlet weak var labelFilmYonetmen: UILabel!
+    
+    var film:Filmler?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let f = film {
+            if let url = URL(string: "http://kasimadalan.pe.hu/filmler/resimler/\(f.film_resim!).png"){
+                DispatchQueue.global().async {
+                    let data = try? Data(contentsOf: url)
+                    DispatchQueue.main.async {
+                        self.imageViewFilmResim.image = UIImage(data: data!)
+                    }
+                }
+            }
+            labelFilmAd.text = f.film_ad
+            labelFimYil.text = String(f.film_yil!)
+            labelFilmKategori.text = f.kategori_ad
+            labelFilmYonetmen.text = f.yonetmen_ad
+        }
+    }
+
+}
---
+++ MovieApplicationFirebase/MovieApp/Kategoriler.swift
@@ -0,0 +1,22 @@
+//
+//  Kategoriler.swift
+//  MovieApp
+//
+//  Created by fatih on 26.02.2022.
+//
+
+import Foundation
+
+class Kategoriler {
+    var kategori_id:String?
+    var kategori_ad:String?
+    
+    init() {
+        
+    }
+    
+    init(kategori_id:String,kategori_ad:String) {
+        self.kategori_id = kategori_id
+        self.kategori_ad = kategori_ad
+    }
+}
